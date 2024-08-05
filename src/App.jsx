ddfcfe82d79d2e7f674d88ba21650cb96fb82ceb
jemy98 <sate85969@gmail.com>
2024-08-05 21:02:49 +0700

    add packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+import AOS from 'aos';
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div>App</div>
   )
