82a9bf4252c38eea570a69633a0f914b4ccd9e0f
jemy98 <sate85969@gmail.com>
2024-08-20 21:25:18 +0700

    add testi and Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Banner from './components/Banner';
+import Testimonial from './components/Testimonial';
 import Services from './components/Services';
 import Footer from './components/Footer';
 import AOS from 'aos';
@@ -20,7 +22,9 @@ const App = () => {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-x-hidden">
       <Navbar />
       <Hero />
+      <Banner />
       <Services />
+      <Testimonial />
       <Footer />
     </div>
   )
